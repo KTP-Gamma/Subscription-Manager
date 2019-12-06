b024b00d63997a3828db2b0e1efd77e103257f5a
jungs1 <jungs1@jungs1-5520.rose-hulman.edu>
2019-12-06 18:49:16 -0500

    work on login

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,16 +2,50 @@
  * @fileoverview
  * Provides interactions for all pages in the UI.
  *
- * @author
+ * @author  Steven Jung
  */
 
 /** namespace. */
 var rh = rh || {};
 
-/** globals */
+rh.beginAuthListening = function () {
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user) {
+			
+			
+			$("#firebaseui-auth-container").hide();
+		} else {
+			// User is signed out.
+			console.log("There is no user.  Nobody is signed in.");
+			// $("#emailPassword").hide();  // Turned off for now.
+			$("#firebaseui-auth-container").show();
+			// $("#userInfo").hide();
+		}
+	});
+}
 
+rh.startFirebaseUi = function () {
+	// FirebaseUI config.
+	var uiConfig = {
+        signInSuccessUrl: '/',
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+          firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+        ],
+      };
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      ui.start("#firebaseui-auth-container", uiConfig);
+}
 
 /* Main */
 $(document).ready(() => {
+	console.log("Ready");
+	rh.beginAuthListening();
+	rh.startFirebaseUi();
 
+	$("#signOut").click((event) => {
+		firebase.auth().signOut()
+	});
 });
