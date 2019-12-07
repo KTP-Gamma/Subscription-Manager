17d5d387410deecf5772f6381698962a3ef59a9d
jungs1 <jungs1@jungs1-5520.rose-hulman.edu>
2019-12-06 19:01:51 -0500

    work on ui

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,18 +11,27 @@ var rh = rh || {};
 rh.beginAuthListening = function () {
 	firebase.auth().onAuthStateChanged(function (user) {
 		if (user) {
-			
+			// User is signed in.
+			$("#uid").html(`<b>uid</b>: ${user.uid}`);
+			$("#email").html(`<b>email</b>: ${user.email}`);
+			$("#displayName").html(`<b>displayName</b>: ${user.displayName}`);
+			$("#photoURL").attr("src", user.photoURL);
+			$("#phoneNumber").html(`<b>phone #</b>: ${user.phoneNumber}`);
+			console.log(user.providerData);
+			console.log("A user IS signed in.  Uid = ", user.uid);
 			
 			$("#firebaseui-auth-container").hide();
+			$("#emailPassword").hide();
+			$("#userInfo").show();
 		} else {
 			// User is signed out.
 			console.log("There is no user.  Nobody is signed in.");
-			// $("#emailPassword").hide();  // Turned off for now.
 			$("#firebaseui-auth-container").show();
-			// $("#userInfo").hide();
+			$("#userInfo").hide();
 		}
 	});
 }
+
 
 rh.startFirebaseUi = function () {
 	// FirebaseUI config.
@@ -42,7 +51,7 @@ rh.startFirebaseUi = function () {
 /* Main */
 $(document).ready(() => {
 	console.log("Ready");
-	rh.beginAuthListening();
+
 	rh.startFirebaseUi();
 
 	$("#signOut").click((event) => {
